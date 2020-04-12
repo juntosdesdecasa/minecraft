9e9f22398bdc4340d3cffe68d0d7db2bc2b7604c
Alvaro del Castillo <alvarodelcastillo@gmail.com>
2020-04-12 18:25:54 +0200

    Delete code moved to mcthings lib

--- src/mcthings/block.py
+++
@@ -1,8 +0,0 @@
-from .object import Object
-
-
-class Block(Object):
-
-    def build(self):
-        self.server.setBlock(self.position.x, self.position.y,
-                             self.position.z, self.block)
